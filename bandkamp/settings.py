2a967fe89acc158b6c5f8ee449c50526756f170b
kalebesampaio <kalebesmp2002@gmail.com>
2022-12-21 18:25:34 -0300

    add url

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['m5entregas5kalebesampaio-production.up.railway.app', '0.0.0.0']
 
 
 # Application definition
