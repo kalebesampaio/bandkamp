8a7387d7d3c37eed9509d038120dae54b0827b1f
kalebesampaio <kalebesmp2002@gmail.com>
2022-12-21 18:47:17 -0300

    corrigindo erros

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -106,7 +106,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 if DATABASE_URL:
     db_from_env = dj_database_url.config(
-        default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+        default=DATABASE_URL)
     DATABASES['default'].update(db_from_env)
     DEBUG = False
 
